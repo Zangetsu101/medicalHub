d8cf77e63513b6b514ed34d1214016122bbfd37a
Zangetsu101 <haidertameem@gmail.com>
2019-06-28 22:04:08 +0600

    changed some structures

---
+++ resources/views/pages/doctors.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+    
+    <section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_1.jpg');" data-stellar-background-ratio="0.5">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center">
+          <div class="col-md-9 ftco-animate text-center">
+            <h1 class="mb-2 bread">Qualified Doctors</h1>
+            <p class="breadcrumbs"><span class="mr-2"><a href="/">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Doctor <i class="ion-ios-arrow-forward"></i></span></p>
+          </div>
+        </div>
+      </div>
+    </section>
+		
+			<section class="ftco-section">
+				<div class="container">
+					<div class="row">
+          @foreach($doctors as $doctor)
+						<div class="col-md-6 col-lg-3">
+							<div class="staff">
+								<div class="text pt-3 text-center">
+									<h3><a href="doctors/{{$doctor->doc_id}}">{{$doctor->name}}</a></h3>
+									<span class="position mb-2">{{$doctor->spec_name}}</span>
+                  <span class="position mb-2">{{$doctor->hospital_name}}</span>
+									</div>
+								</div>
+							</div>
+            @endforeach
+						</div>
+					</div>
+			</section>
+
+@endsection
