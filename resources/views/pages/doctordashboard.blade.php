db40b5478e70d836de09974fb0490a0132586d0c
fuad113 <anfuad96@yahoo.com>
2019-07-08 01:01:05 +0600

    doctor dashboard working

--- resources/views/pages/doctordashboard.blade.php
+++ resources/views/pages/doctordashboard.blade.php
@@ -8,23 +8,11 @@
 
             <div class="card-box">
                 <div class="info">
-<<<<<<< HEAD:resources/views/home.blade.php
-                Patient Id: {{$patient->patient_id}}  <br>
-                Patient Name: {{$patient->name}} <br>
-                Gender: @if($patient->gender=='m')
-                            Male
-                        @else  
-                            Female
-                        @endif
-                        <br>
-                Date of Birth: {{$patient->dob}} 
-=======
                 Doctor Id: {{$doctor->doc_id}}  <br>
                 Name: {{$doctor->name}} <br>
                 Designation: {{$doctor->designation}} <br>
                 Office: {{$doctor->room_no}},{{$doctor->hospital->name}} <br>
                 Spec: {{$doctor->spec->spec_name}}
->>>>>>> 262d8072d13c080ffb3c2faee7700877b52ef18a:resources/views/pages/doctordashboard.blade.php
                 </div>
             
                 <div class="patientmenu">
