44e6e4ec3c52e084fdb623e211d0f4a27b7f21b1
Zangetsu101 <haidertameem@gmail.com>
2019-07-08 15:28:33 +0600

    conflict resolve again

--- app/Appointment.php
+++ app/Appointment.php
@@ -8,16 +8,10 @@ class Appointment extends Model
 {
     //
     protected $primaryKey='appt_id';
-<<<<<<< HEAD
     public $timestamps=false;
 
     public function doctor()
     {
         return $this->belongsTo('App\Doctor','doc_id','doc_id');
     }
-=======
-
-    
-
->>>>>>> e12f5c71d26c1d4e3543093b2ace0f4fbe9bb711
 }
