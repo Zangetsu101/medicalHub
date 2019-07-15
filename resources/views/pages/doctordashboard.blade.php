6f92beccb29109121e8cc1f712a2615194807e84
fuad113 <anfuad96@yahoo.com>
2019-07-15 14:53:29 +0600

    doctor dashboard working

--- resources/views/pages/doctordashboard.blade.php
+++ resources/views/pages/doctordashboard.blade.php
@@ -2,12 +2,21 @@
 
 @section('content')
 
-<div class="vertical-menu">
-  <a href="{{route('dashboard')}}" class="active">Home</a>
-  <a href="{{route('doctodayschedule')}}">Today Schedule</a>
-  <a href="{{route('criticalpatients')}}">Critical Patients</a>
-  <a href="{{route('apptfortoday')}}">Appointments For Today</a>  
-  <a href="{{route('emergencyops')}}">Emergency Operations</a>
+<div class="row">
+
+  <div class="column">
+    <div class="vertical-menu">
+      <a href="{{route('dashboard')}}" class="active">Home</a>
+      <a href="{{route('doctodayschedule')}}">Today Schedule</a>
+      <a href="{{route('criticalpatients')}}">Critical Patients</a>
+      <a href="{{route('apptfortoday')}}">Appointments For Today</a>  
+      <a href="{{route('emergencyops')}}">Emergency Operations</a>
+    </div>
+  </div>
+
+
+
+
 </div>
 
 @endsection
