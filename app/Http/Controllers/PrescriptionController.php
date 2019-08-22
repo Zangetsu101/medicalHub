b23faa50d52a33abfa4fd5fb6d30e4a446214632
fuad113 <anfuad96@yahoo.com>
2019-08-23 03:00:43 +0600

    medicine added to the prescription

--- app/Http/Controllers/PrescriptionController.php
+++ app/Http/Controllers/PrescriptionController.php
@@ -7,6 +7,7 @@ use App\Prescription;
 use App\Patient;
 use App\Doctor;
 use App\Appointment;
+use App\Medicine;
 
 class PrescriptionController extends Controller
 {
@@ -30,7 +31,13 @@ class PrescriptionController extends Controller
      */
     public function create(Request $request)
     {
-        
+       $this->validate($request,[
+            'appt_id'=>'required',
+            'weight'=>'required',
+            'bplow'=>'required',
+            'bphigh'=>'required',
+            'complain'=>'required'
+       ]); 
        $pres=new Prescription;
 
        $id=count(Prescription::all())+1;
@@ -40,8 +47,138 @@ class PrescriptionController extends Controller
        $pres->bp_low=$request->input('bplow');
        $pres->bp_high=$request->input('bphigh');
        $pres->cond=$request->input('complain');  
-
        $pres->save();
+
+       
+       if(!empty($request->input('m1')))
+       {
+           $id2=count(Medicine::all())+1;
+           $med=new Medicine;
+
+           $med->id=$id2;
+           $med->prescription_id=$id;
+           $med->name=$request->input('m1');
+           $med->duration=$request->input('d1');
+           $med->dosage=$request->input('dos1');
+           $med->save();
+       }
+
+       if(!empty($request->input('m2')))
+       {
+           $id2=count(Medicine::all())+1;
+           $med=new Medicine;
+
+           $med->id=$id2;
+           $med->prescription_id=$id;
+           $med->name=$request->input('m2');
+           $med->duration=$request->input('d2');
+           $med->dosage=$request->input('dos2');
+           $med->save();
+       }
+
+       if(!empty($request->input('m3')))
+       {
+           $id2=count(Medicine::all())+1;
+           $med=new Medicine;
+
+           $med->id=$id2;
+           $med->prescription_id=$id;
+           $med->name=$request->input('m3');
+           $med->duration=$request->input('d3');
+           $med->dosage=$request->input('dos3');
+           $med->save();
+       }
+
+       if(!empty($request->input('m4')))
+       {
+           $id2=count(Medicine::all())+1;
+           $med=new Medicine;
+
+           $med->id=$id2;
+           $med->prescription_id=$id;
+           $med->name=$request->input('m4');
+           $med->duration=$request->input('d4');
+           $med->dosage=$request->input('dos4');
+           $med->save();
+       }
+
+       if(!empty($request->input('m5')))
+       {
+           $id2=count(Medicine::all())+1;
+           $med=new Medicine;
+
+           $med->id=$id2;
+           $med->prescription_id=$id;
+           $med->name=$request->input('m5');
+           $med->duration=$request->input('d5');
+           $med->dosage=$request->input('dos5');
+           $med->save();
+       }
+
+       if(!empty($request->input('m6')))
+       {
+           $id2=count(Medicine::all())+1;
+           $med=new Medicine;
+
+           $med->id=$id2;
+           $med->prescription_id=$id;
+           $med->name=$request->input('m6');
+           $med->duration=$request->input('d6');
+           $med->dosage=$request->input('dos6');
+           $med->save();
+       }
+
+       if(!empty($request->input('m7')))
+       {
+           $id2=count(Medicine::all())+1;
+           $med=new Medicine;
+
+           $med->id=$id2;
+           $med->prescription_id=$id;
+           $med->name=$request->input('m7');
+           $med->duration=$request->input('d7');
+           $med->dosage=$request->input('dos7');
+           $med->save();
+       }
+
+       if(!empty($request->input('m8')))
+       {
+           $id2=count(Medicine::all())+1;
+           $med=new Medicine;
+
+           $med->id=$id2;
+           $med->prescription_id=$id;
+           $med->name=$request->input('m8');
+           $med->duration=$request->input('d8');
+           $med->dosage=$request->input('dos8');
+           $med->save();
+       }
+
+       if(!empty($request->input('m9')))
+       {
+           $id2=count(Medicine::all())+1;
+           $med=new Medicine;
+
+           $med->id=$id2;
+           $med->prescription_id=$id;
+           $med->name=$request->input('m9');
+           $med->duration=$request->input('d9');
+           $med->dosage=$request->input('dos9');
+           $med->save();
+       }
+
+       if(!empty($request->input('m10')))
+       {
+           $id2=count(Medicine::all())+1;
+           $med=new Medicine;
+
+           $med->id=$id2;
+           $med->prescription_id=$id;
+           $med->name=$request->input('m10');
+           $med->duration=$request->input('d10');
+           $med->dosage=$request->input('dos10');
+           $med->save();
+       }
 
        return redirect()->route('upcomingappts');
 
